c1c0dc3a83d9cb492cb369fda4d3f71908c1982a
Sergey <sadhorsephile@gmail.com>
2022-03-01 00:23:45 +0300

    SPO-11056 feat: add main screen ui
    Add main screen ui and provide it with data from widget model

--- lib/core/presentation/screens/main/ui.dart
+++ lib/core/presentation/screens/main/ui.dart
@@ -1,18 +1,175 @@
 import 'package:currency_exchange/core/presentation/screens/main/wmodel.dart';
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shimmer/shimmer.dart';
 
 /// Виджет главного экрана
-class MainScreen extends ElementaryWidget<MainScreenWidgetModel> {
+class MainScreen extends ElementaryWidget<IMainScreenWidgetModel> {
   const MainScreen({Key? key}) : super(mainScreenWidgetModelFactory, key: key);
+
   @override
-  Widget build(MainScreenWidgetModel wm) {
+  Widget build(IMainScreenWidgetModel wm) {
+    final uiProperties = wm.uiElementsProperties;
+
+    final debit = _CurrencyTextBox(
+      textFieldState: wm.debitTextFieldState,
+      uiProperties: uiProperties,
+      hint: uiProperties.debitHint,
+      inputFormatter: wm.inputFormatter,
+    );
+
+    final credit = _CurrencyTextBox(
+      textFieldState: wm.creditTextFieldState,
+      uiProperties: uiProperties,
+      hint: uiProperties.creditHint,
+      inputFormatter: wm.inputFormatter,
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main screen'),
+      appBar: AppBar(title: Text(uiProperties.appBarTitle)),
+      backgroundColor: uiProperties.backgroundColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: uiProperties.horizontalPadding,
+            vertical: uiProperties.verticalPadding,
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: uiProperties.spacing),
+              if (wm.orientation == Orientation.portrait)
+                Column(
+                  children: [
+                    debit,
+                    SizedBox(height: uiProperties.spacing),
+                    credit,
+                  ],
+                )
+              else
+                Row(
+                  children: [
+                    Expanded(child: debit),
+                    SizedBox(width: uiProperties.spacing),
+                    Expanded(child: credit),
+                  ],
+                ),
+            ],
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Content'),
+    );
+  }
+}
+
+/// Элемент, представляющий текстовое поле и кнопку переключения валюты
+class _CurrencyTextBox extends StatelessWidget {
+  final ListenableState<EntityState<CurrencyTextFieldDto>> textFieldState;
+  final UiElementsProperties uiProperties;
+  final TextInputFormatter inputFormatter;
+  final String hint;
+
+  const _CurrencyTextBox({
+    required this.textFieldState,
+    required this.hint,
+    required this.uiProperties,
+    required this.inputFormatter,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return EntityStateNotifierBuilder<CurrencyTextFieldDto>(
+      listenableEntityState: textFieldState,
+      loadingBuilder: (_, state) => _buildBody(true, state),
+      builder: (_, state) => _buildBody(false, state),
+      errorBuilder: (_, ex, state) => _buildBody(
+        false,
+        state,
+        ex.toString(),
+      ),
+    );
+  }
+
+  Widget _buildBody(
+    bool isLoading,
+    CurrencyTextFieldDto? state, [
+    String? errorMessage,
+  ]) {
+    final textFieldWithButton = Stack(
+      alignment: Alignment.center,
+      children: [
+        TextField(
+          controller: state?.controller,
+          enabled: !isLoading,
+          keyboardType: const TextInputType.numberWithOptions(decimal: true),
+          inputFormatters: [inputFormatter],
+          decoration: uiProperties.textFieldDecoration.copyWith(hintText: hint),
+        ),
+        if (state?.currencySymbol != null)
+          Positioned(
+            right: 10,
+            child: _RoundCurrencyButton(label: state!.currencySymbol),
+          ),
+      ],
+    );
+
+    final body = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        textFieldWithButton,
+        if (errorMessage != null)
+          Row(
+            children: [
+              Text(
+                errorMessage,
+                style: TextStyle(color: uiProperties.errorTextColor),
+              ),
+              SizedBox(width: uiProperties.spacing),
+              Icon(
+                Icons.refresh,
+                color: uiProperties.refreshIconColor,
+              ),
+            ],
+          ),
+      ],
+    );
+
+    return isLoading
+        ? Shimmer.fromColors(
+            baseColor: uiProperties.shimmerBaseColor,
+            highlightColor: uiProperties.shimmerHighlightColor,
+            child: body,
+          )
+        : body;
+  }
+}
+
+/// Кнопка переключения валюты
+class _RoundCurrencyButton extends StatelessWidget {
+  final String label;
+
+  const _RoundCurrencyButton({required this.label, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Material(
+        color: Colors.blue,
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            width: 35,
+            height: 35,
+            decoration: const BoxDecoration(shape: BoxShape.circle),
+            child: Center(
+              child: Text(
+                label,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/core/presentation/screens/main/wmodel.dart
+++ lib/core/presentation/screens/main/wmodel.dart
@@ -1,13 +1,127 @@
 import 'package:currency_exchange/core/presentation/screens/main/model.dart';
-import 'package:currency_exchange/core/presentation/screens/main/ui.dart';
 import 'package:elementary/elementary.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 /// Фабрика виджет-модели главного экрана
-MainScreenWidgetModel mainScreenWidgetModelFactory(BuildContext context) =>
-    MainScreenWidgetModel(MainScreenModel());
+IMainScreenWidgetModel mainScreenWidgetModelFactory(BuildContext _) =>
+    MainScreenWidgetModel(
+      MainScreenModel(),
+      TextEditingController(),
+      TextEditingController(),
+    );
 
 /// Виджет-модель главного экрана
-class MainScreenWidgetModel extends WidgetModel<MainScreen, MainScreenModel> {
-  MainScreenWidgetModel(MainScreenModel model) : super(model);
+class MainScreenWidgetModel extends IMainScreenWidgetModel {
+  /// Контроллер текстового поля списания
+  final TextEditingController _debitController;
+  /// Контроллер текстового поля зачисления
+  final TextEditingController _creditController;
+
+  final _debitStateNotifier = EntityStateNotifier<CurrencyTextFieldDto>();
+  final _creditStateNotifier = EntityStateNotifier<CurrencyTextFieldDto>();
+
+  @override
+  ListenableState<EntityState<CurrencyTextFieldDto>> get debitTextFieldState =>
+      _debitStateNotifier;
+  @override
+  ListenableState<EntityState<CurrencyTextFieldDto>> get creditTextFieldState =>
+      _creditStateNotifier;
+  @override
+  Orientation get orientation => MediaQuery.of(context).orientation;
+
+  @override
+  UiElementsProperties get uiElementsProperties => UiElementsProperties(
+        appBarTitle: 'Currency exchange',
+        debitHint: 'Списание',
+        creditHint: 'Зачисление',
+        horizontalPadding: 20.0,
+        verticalPadding: 10.0,
+        spacing: 10.0,
+        backgroundColor: Colors.grey[100]!,
+        errorTextColor: Colors.red,
+        refreshIconColor: Colors.blue,
+        shimmerBaseColor: Colors.grey[100]!,
+        shimmerHighlightColor: Colors.grey[300]!,
+        textFieldDecoration: const InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+        ),
+      );
+
+  MainScreenWidgetModel(
+    MainScreenModel model,
+    this._debitController,
+    this._creditController,
+  ) : super(model) {
+    _debitStateNotifier.content(CurrencyTextFieldDto(_debitController, '₽'));
+    _creditStateNotifier.content(CurrencyTextFieldDto(_creditController, r'$'));
+  }
+}
+
+/// Интерфейс виджет-модели
+abstract class IMainScreenWidgetModel extends WidgetModel {
+  /// Источник состояния текстового поля списания
+  ListenableState<EntityState<CurrencyTextFieldDto>> get debitTextFieldState;
+  /// Источник состояния текстового поля зачисления
+  ListenableState<EntityState<CurrencyTextFieldDto>> get creditTextFieldState;
+  /// Текущее состояние ориентации устройства
+  Orientation get orientation;
+  UiElementsProperties get uiElementsProperties;
+  /// Форматтер для поля ввода количества валюты (позволяет вводить только числа и знак разделения '.')
+  TextInputFormatter get inputFormatter =>
+      FilteringTextInputFormatter.allow(RegExp(r'[\d\.]'));
+
+  IMainScreenWidgetModel(ElementaryModel model) : super(model);
+}
+
+/// Дата-класс, держащий в себе данные о валюте и ее введенном количестве
+class CurrencyTextFieldDto {
+  final TextEditingController controller;
+  final String currencySymbol;
+
+  CurrencyTextFieldDto(this.controller, this.currencySymbol);
+}
+
+/// Набор значений, задающих свойство ui-элементов
+class UiElementsProperties {
+  /// Текст шапки страницы
+  final String appBarTitle;
+  /// Хинт текстового поля списания
+  final String debitHint;
+  /// Хинт текстового поля зачисления
+  final String creditHint;
+  /// Значение горизонтального отступа
+  final double horizontalPadding;
+  /// Значение вертикального отступа
+  final double verticalPadding;
+  /// Значение отступа между элементами
+  final double spacing;
+  /// Цвет заднего фона экрана
+  final Color backgroundColor;
+  /// Конфигурация внешнего вида текстового поля
+  final InputDecoration textFieldDecoration;
+  /// Цвет текста ошибки
+  final Color errorTextColor;
+  /// Цвет иконки повтора запроса
+  final Color refreshIconColor;
+  /// Базовый цвет шиммера
+  final Color shimmerBaseColor;
+  /// Цвет подсветки шиммера
+  final Color shimmerHighlightColor;
+  
+  UiElementsProperties({
+    required this.appBarTitle,
+    required this.debitHint,
+    required this.creditHint,
+    required this.horizontalPadding,
+    required this.verticalPadding,
+    required this.spacing,
+    required this.backgroundColor,
+    required this.textFieldDecoration,
+    required this.errorTextColor,
+    required this.refreshIconColor,
+    required this.shimmerBaseColor,
+    required this.shimmerHighlightColor,
+  });
 }
