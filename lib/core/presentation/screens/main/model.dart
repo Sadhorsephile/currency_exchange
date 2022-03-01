68a7b1ad632f8328ee4c3e40915dd51ce780dc5c
Sergey <sadhorsephile@gmail.com>
2022-03-01 12:24:40 +0300

    SPO-11057 feat: add basic model methods

--- lib/core/presentation/screens/main/model.dart
+++ lib/core/presentation/screens/main/model.dart
@@ -1,4 +1,21 @@
 import 'package:elementary/elementary.dart';
 
 /// Модель главного экрана
-class MainScreenModel extends ElementaryModel {}
+class MainScreenModel extends ElementaryModel {
+  final _mockCurrency = 120;
+
+  /// Метод загрузки данных для экрана
+  Future<void> loadData() {
+    return Future<void>.delayed(const Duration(seconds: 2));
+  }
+
+  /// Метод преобразования из валюты списания в валюту зачисления
+  double fromDebitToCredit(double debit) {
+    return debit * _mockCurrency;
+  }
+
+  /// Метод преобразования из валюты зачисления в валюту списания
+  double fromCreditToDebit(double credit) {
+    return credit / _mockCurrency;
+  }
+}
