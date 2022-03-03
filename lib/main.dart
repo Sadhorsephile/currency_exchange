9645c2f994ccefafa705adf25c03e5c80cdd00f7
Sergey <sadhorsephile@gmail.com>
2022-03-03 11:11:43 +0300

    SPO-11067 feat: add network client

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:currency_exchange/core/data/network/client.dart';
 import 'package:currency_exchange/core/presentation/screens/main/ui.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NetworkClient.initCacheOptions();
   runApp(const MyApp());
 }
 
@@ -10,10 +15,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Currency Exchange',
-      home: MainScreen(),
+    return Provider<NetworkClient>(
+      create: (_) => NetworkClient(Dio()),
+      child: const MaterialApp(
+        title: 'Currency Exchange',
+        home: MainScreen(),
+      ),
     );
   }
 }
-
